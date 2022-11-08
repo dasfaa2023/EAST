bde5f705ddc6b3488f291e8676b6b63e901e9f02
dasfaa <111172959+dasfaa2023@users.noreply.github.com>
2022-11-08 17:03:26 +0800

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # sh scripts for EAST
 
+
 # EAST*-PEMS08
 python3 train.py --dataset PEMS08 --K 4 --max_epoch 1000 --batch_size 32 --node_num 170 --learning_rate 0.001 --patience 20 --emb_dim 16 --encoder_layer 1 --decoder_layer 1
 
